43163b0a26bef1083dfa332a7eb18a4e72edf9c9
Michael Tangy <mike.tangy.91@gmail.com>
2017-01-31 13:58:30 -0600

    Create wc.c

---
+++ wc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main(){
+
+        int words, chars, lines;
+        words = chars = 0;
+        lines =1;
+    FILE* f = fopen("wc.txt","r");
+        int c = 0;
+        while((c= fgetc(f)) != EOF){
+                if(c == '\n'){
+                        lines++;
+                }else{
+                        chars++;
+                        while(c = fgetc(f)){
+                                chars++;
+                                if(c == ' ' || c == '\t')
+                                        break;
+                                if(c == '\n'){
+                                        lines++;
+                                        break;
+                                }
+                        }
+                        words++;
+                }
+        }
+        printf("char: %d words: %d lines: %d",chars,words,lines);
+}
